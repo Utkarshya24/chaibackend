bfc1c423e4de7e734af923bbd940398325e684a2
Utkarsh Yadav <165546567+Utkarshya24@users.noreply.github.com>
2024-10-05 01:34:09 -0700

    Update index.js , check port number in console

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express')
 
 const app = express()
 
-const port = 4000
+const port = process.env.PORT || 4000
 
 const githubData = {
     "login": "hiteshchoudhary",
@@ -61,6 +61,6 @@ app.get('/github', (req, res) => {
     res.json(githubData)
 })
 
-app.listen(process.env.PORT, () => {
+app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
